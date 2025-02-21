2b6ab73bfa67929ae28f58d374a56c2076824e44
MOYU <76060163+Moyu117@users.noreply.github.com>
2025-02-21 23:58:55 +0800

    Update NetworkInitializer.h
    适配linux

--- client/NetworkInitializer.h
+++ client/NetworkInitializer.h
@@ -4,6 +4,16 @@
 #ifdef _WIN32
 #include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define SOCKET int
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
+#define closesocket close //pour linux
 #endif
 
 class NetworkInitializer {
@@ -14,7 +24,7 @@ private:
 public:
     static NetworkInitializer* getInstance();
 
-    // ½û¿½±´
+    // Â½Ã»Â¿Â½Â±Â´
     NetworkInitializer(const NetworkInitializer&) = delete;
     NetworkInitializer& operator=(const NetworkInitializer&) = delete;
 };
